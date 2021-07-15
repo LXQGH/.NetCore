052911751b514ff49e198fbc7263d633cecf556c
隆秀权 <2728322034@qq.com>
2021-07-15 22:38:36 +0800

    添加初始化种子数据

--- FakeXiecheng.API/FakeXiecheng.API/Database/AppDbContext.cs
+++ FakeXiecheng.API/FakeXiecheng.API/Database/AppDbContext.cs
@@ -1,8 +1,11 @@
 ﻿using FakeXiecheng.API.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace FakeXiecheng.API.Database
@@ -15,5 +18,26 @@ namespace FakeXiecheng.API.Database
         }
         public DbSet<TouristRoute> TouristRoutes { get; set; }
         public DbSet<TouristRoutePicture> TouristRoutePictures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //种子数据
+            //modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
+            //{
+            //    Id=Guid.NewGuid(),
+            //    Title="ceshititle",
+            //    Description="shuoming",
+            //    OriginalPrice=0,
+            //    CreateTime=DateTime.UtcNow
+            //});
+            var touristRouteJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutesMockData.json");
+            IList<TouristRoute> touristRoutes = JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRouteJsonData);
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);
+
+            var touristRoutePictureJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutePicturesMockData.json");
+            IList<TouristRoutePicture> touristRoutePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(touristRoutePictureJsonData);
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ FakeXiecheng.API/FakeXiecheng.API/Migrations/20210715142053_DataSeeding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FakeXiecheng.API.Migrations
+{
+    public partial class DataSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TouristRoutes",
+                columns: new[] { "Id", "CreateTime", "DepartureTime", "Description", "DisciuntPresnt", "Features", "Fees", "Notes", "OriginalPrice", "Title", "UpdateTime" },
+                values: new object[] { new Guid("bf1f7a5f-4db6-4d89-8e05-3b5602839201"), new DateTime(2021, 7, 15, 14, 20, 53, 478, DateTimeKind.Utc).AddTicks(3302), null, "shuoming", null, null, null, null, 0m, "ceshititle", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TouristRoutes",
+                keyColumn: "Id",
+                keyValue: new Guid("bf1f7a5f-4db6-4d89-8e05-3b5602839201"));
+        }
+    }
+}
