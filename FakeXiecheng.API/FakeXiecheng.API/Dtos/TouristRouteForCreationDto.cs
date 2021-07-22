a5d973be55fa8e037a9e349fec2476581e83fb80
隆秀权 <2728322034@qq.com>
2021-07-22 22:07:36 +0800

    添加数据验证

--- FakeXiecheng.API/FakeXiecheng.API/Dtos/TouristRouteForCreationDto.cs
+++ FakeXiecheng.API/FakeXiecheng.API/Dtos/TouristRouteForCreationDto.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using FakeXiecheng.API.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FakeXiecheng.API.Dtos
 {
-    public class TouristRouteForCreationDto
+    [TouristRouteTitleMustBeDifferentFromDescriptionAttribute]
+    public class TouristRouteForCreationDto //:IValidatableObject
     {
+        [Required(ErrorMessage ="title 不可为空")]
+        [MaxLength(100)]
         public string Title { get; set; }
+        [Required]
+        [MaxLength(1500)]
         public string Description { get; set; }
         //计算方式：原价*折扣
         public decimal Price { get; set; }
@@ -25,5 +32,16 @@ namespace FakeXiecheng.API.Dtos
         public string DepartureCity { get; set; }
         public ICollection<TouristRoutePictureForCreationDto> TouristRoutePictures { get; set; }
          = new List<TouristRoutePictureForCreationDto>();
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if(Title==Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "路线名称必须与路线描述不同",
+        //            new[] { "TouristRouteForCreationDto" }
+        //            );
+        //    }
+        //}
     }
 }
